084210b56d384ae4609b89eb84d44c558af09460
Tony's Test App <chen.tony.h@gmail.com>
2017-04-27 20:58:57 -0700

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Tony Chen on 4/27/17.
+//  Copyright © 2017 TonyHChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
